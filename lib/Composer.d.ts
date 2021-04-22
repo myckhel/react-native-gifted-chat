f2b664677ba51085d8866c7ca12eff17070dd82f
myckhel <myckhel1@hotmail.com>
2021-04-22 18:48:27 +0100

    gitpkg

---
+++ lib/Composer.d.ts
@@ -0,0 +1,51 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { TextInputProps } from 'react-native';
+export interface ComposerProps {
+    composerHeight?: number;
+    text?: string;
+    placeholder?: string;
+    placeholderTextColor?: string;
+    textInputProps?: Partial<TextInputProps>;
+    textInputStyle?: TextInputProps['style'];
+    textInputAutoFocus?: boolean;
+    keyboardAppearance?: TextInputProps['keyboardAppearance'];
+    multiline?: boolean;
+    disableComposer?: boolean;
+    onTextChanged?(text: string): void;
+    onInputSizeChanged?(layout: {
+        width: number;
+        height: number;
+    }): void;
+}
+export declare function Composer(props: ComposerProps): React.ReactElement;
+export declare namespace Composer {
+    var defaultProps: {
+        composerHeight: number | undefined;
+        text: string;
+        placeholderTextColor: string;
+        placeholder: string;
+        textInputProps: null;
+        multiline: boolean;
+        disableComposer: boolean;
+        textInputStyle: {};
+        textInputAutoFocus: boolean;
+        keyboardAppearance: string;
+        onTextChanged: () => void;
+        onInputSizeChanged: () => void;
+    };
+    var propTypes: {
+        composerHeight: PropTypes.Requireable<number>;
+        text: PropTypes.Requireable<string>;
+        placeholder: PropTypes.Requireable<string>;
+        placeholderTextColor: PropTypes.Requireable<string>;
+        textInputProps: PropTypes.Requireable<object>;
+        onTextChanged: PropTypes.Requireable<(...args: any[]) => any>;
+        onInputSizeChanged: PropTypes.Requireable<(...args: any[]) => any>;
+        multiline: PropTypes.Requireable<boolean>;
+        disableComposer: PropTypes.Requireable<boolean>;
+        textInputStyle: PropTypes.Requireable<number | boolean | object>;
+        textInputAutoFocus: PropTypes.Requireable<boolean>;
+        keyboardAppearance: PropTypes.Requireable<string>;
+    };
+}
